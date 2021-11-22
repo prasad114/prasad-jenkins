a1eac5d3487906fc872d14d22c4f7db7a1c30ce3
root <root@ip-172-31-22-31.us-east-2.compute.internal>
2021-11-22 13:03:19 +0000

    1st

---
+++ src/main/java/jenkins-project/App.java
@@ -0,0 +1,13 @@
+package jenkins-project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
